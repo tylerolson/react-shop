d3526191274003238f7a93b82a47c16837e03ee8
Tyler Olson <tolson519@gmail.com>
2019-07-03 17:32:15 -0700

    Updates

---
+++ precache-manifest.e30ac3c53dcf9d7fef493e7dfd938a82.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af4f181a2f1a85aab2d81fd1ab8d7d43",
+    "url": "/react-shop/index.html"
+  },
+  {
+    "revision": "484573cd10e4bf485b8d",
+    "url": "/react-shop/static/css/main.8374c741.chunk.css"
+  },
+  {
+    "revision": "87c4f02bee46e42c2877",
+    "url": "/react-shop/static/js/2.c4ad3bc2.chunk.js"
+  },
+  {
+    "revision": "484573cd10e4bf485b8d",
+    "url": "/react-shop/static/js/main.84f791b6.chunk.js"
+  },
+  {
+    "revision": "75bd495123a48ea82bb9",
+    "url": "/react-shop/static/js/runtime~main.0bd8b961.js"
+  }
+]);
